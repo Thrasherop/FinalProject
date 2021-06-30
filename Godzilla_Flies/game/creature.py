cb41187050ae6dce6b1c8169bbfb275bb2a714c5
Brigham <brigham4210@gmail.com>
2021-07-01 01:12:18 +0800

    bri

--- Godzilla_Flies/game/creature.py
+++ Godzilla_Flies/game/creature.py
@@ -1,11 +1,13 @@
 from random import randint
 from arcade.physics_engines import PhysicsEngineSimple
 from game.constants import *
+import arcade.sprite
 
 
 
-class Creature:
+class Creature(arcade.sprite):
     def __init__(self, sprite, speed):
+        super().__init__()
         self.sprite = sprite
         self.sprite.center_x = SCREEN_WIDTH / 2
         self.sprite.center_y = SCREEN_HEIGHT / 2
